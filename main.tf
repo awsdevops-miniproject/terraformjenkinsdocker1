9ee01e78caba9516363b4e933a845d4a8c39c736
josephelishaimmanuel-pixel <josephelishaimmanuel@gmail.com>
2026-07-10 13:43:07 +0530

    Initial Terraform and web application

---
+++ main.tf
@@ -0,0 +1,50 @@
+resource "aws_security_group" "web_sg" {
+
+  name = "terraform-web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "devops_server" {
+
+  ami = data.aws_ami.ubuntu.id   # Ubuntu 22.04 in ap-south-1 (verify the latest AMI before use)
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.web_sg.id
+  ]
+
+  tags = {
+    Name = "DevOps-Server"
+  }
+}
+
